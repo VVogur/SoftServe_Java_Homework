1ba19db667528b1fa799843dbc6542ed43a3805c
VVogur <36006268+VVogur@users.noreply.github.com>
2021-05-04 12:58:18 +0300

    complete task 2

---
+++ src/homework5/TaskTwoFibonacci.java
@@ -0,0 +1,21 @@
+package homework5;
+
+public class TaskTwoFibonacci {
+    public static int fibonacciRecursion(int n){
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        return fibonacciRecursion(n-2) + fibonacciRecursion(n-1);
+    }
+    public static void main(String args[]) {
+        int maxNumber = 20;
+        System.out.print("Fibonacci Series of "+maxNumber+" numbers: ");
+        for(int i = 0; i < maxNumber; i++){
+            System.out.print(fibonacciRecursion(i) +" ");
+        }
+    }
+}
+
